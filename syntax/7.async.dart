86b8dc562af11dcdb11f4cbb6728a5898001dee2
yzh <yangzhenghan@lonwin.org>
2020-03-12 13:52:40 +0800

    add step

--- syntax/7.async.dart
+++ syntax/7.async.dart
@@ -11,3 +11,36 @@
 /**
  * 异步
  */
+
+import 'dart:math';
+
+// 同步生成器
+Iterable<int> generateNum(int n) sync* {
+  int i = 0;
+  while (i < n) {
+    yield Random().nextInt(0xFFFF);
+  }
+}
+
+// 异步生成器
+Stream<int> generateNumAsync(int n) async* {
+  int i = 0;
+  while (i++ < n) {
+    yield Random().nextInt(0xFFFF);
+  }
+}
+
+Stream<int> rev(int n) async* {
+  if (n > 0) {
+    yield n;
+    yield* rev(n - 1);
+  }
+}
+
+Future<String> lookupVersionAsync() async => '1.0.0';
+
+Future main(List<String> args) async {
+  await for (var item in rev(10)) {
+    print(item);
+  }
+}
